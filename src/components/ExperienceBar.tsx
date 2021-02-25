abf47aaf747951dfd2de034299f4c6e9d4e77948
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-02-25 12:56:39 -0300

    Add experiencebar dynamic with

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -5,13 +5,15 @@ import styles from '../styles/components/ExperienceBar.module.css'
 export function ExperienceBar() {
     const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext)
 
+    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }}  />
+                <div style={{ width: `${percentToNextLevel}%`}}  />
 
-                <span className={styles.currentExperience} style={{ left: '50%' }}>
+                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
                     {currentExperience}xp
                 </span>
             </div>
