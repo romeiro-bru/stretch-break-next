60adcfd2e712095b560d8b8db431ae6570644f87
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-02-25 12:45:12 -0300

    Calculate level experience

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -10,6 +10,7 @@ interface Challenge {
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
+    experienceToNextLevel: number;
     challengesCompleted: number;
     activeChallenge: Challenge;
     levelUp: () => void;
@@ -29,6 +30,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [challengesCompleted, setChallengesCompleted] = useState(0)
 
     const [activeChallenge, setActiveChallenge] = useState(null)
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
     function levelUp() {
         setLevel(level + 1)
@@ -54,6 +57,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
             startNewChallenge,
             activeChallenge,
             resetChallenge,
+            experienceToNextLevel,
          }}
         >
             {children}
