6bdb09c3743b7330323150d1e2cb465ec8e61f51
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-02-26 16:45:41 -0300

    Add interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,21 @@ import { ChallengeBox } from '../components/ChallengeBox';
 import { CountdownProvider } from '../contexts/CountdownContext';
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home(props) {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   console.log(props)
   return (
-    <ChallengesProvider>
+    <ChallengesProvider
+      level={props.level}
+      currentExperiece={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+
       <div className={styles.container}>
         <Head>
           <title>Início | stretch.break</title>
@@ -50,9 +61,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   return {
     props: {
-      level,
-      currentExperience,
-      challengesCompleted
+      level: Number(level),
+      currentExperience: Number(level),
+      challengesCompleted: Number(level)
     }
   }
 }
